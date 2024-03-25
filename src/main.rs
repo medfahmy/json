9ab6815c2de6137130cc816820a0eec2b39b0e40
medfahmy <medfahmy99@gmail.com>
2024-03-25 10:23:41 +0000

    query loop

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use json::Lexer;
+use std::{fs::read_to_string, io::{stdin, stdout, Write}};
+use json::Parser;
 
 fn main() {
-    let input = "\"hello\": -";
-    let result = Lexer::lex(input);
+    let stdin = stdin();
+    let mut stdout = stdout();
+    let path = std::env::args().nth(1).expect("no file path provided");
+    let file = read_to_string(path).expect("error opening file");
+    let mut parser = Parser::new(&file);
+    let value = parser.parse().expect("error parsing json");
 
-    println!("{:?}", result);
+    loop {
+        print!(">> ");
+        let _ = stdout.flush();
+        let mut buf = String::new();
+        stdin.read_line(&mut buf).expect("error reading input");
+
+        println!("{}", value.query(buf));
+    }
 }
